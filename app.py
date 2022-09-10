7debd19297aed6a1ad54653b6024cabf32df8be2
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2022-09-10 03:23:06 +0200

    Add code

---
+++ app.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+import pyautogui
+
+# get audio from the microphone                                                                       
+r = sr.Recognizer()   
+
+while True:
+    with sr.Microphone() as source:                                                                       
+        print("Speak:")       
+        r.adjust_for_ambient_noise(source)                                                                            
+        audio = r.listen(source)   
+
+    try:
+        txt = r.recognize_google(audio)
+
+        print(">>" + txt)
+
+        if txt == 'right':
+            print('go right!!')
+            pyautogui.move(30, 0)
+        elif txt == 'left' or txt == 'lift':
+            print('go left!!')
+            pyautogui.move(-30, 0)
+        elif txt == 'top' or txt == 'up' or txt == 'stop':
+            print('go top!!')
+            pyautogui.move(0, -30)
+        elif txt == 'down' or txt == 'Darwin':
+            print('go Down!!')
+            pyautogui.move(0, 30)
+        elif txt == 'go left' or txt == 'go lift':
+            pyautogui.move(-50, 0)
+        elif txt == 'go right':
+            pyautogui.move(50, 0)
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
